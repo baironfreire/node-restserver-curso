2b4bfcba413771d17886fc695fe820d8f67c7b70
Bairon <bairon.f5@gmail.com>
2019-06-25 15:46:43 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 
-    urlDB = 'mongodb+srv://desarrollo:Igs9aizqJQbr0jTT@cluster0-hd3ur.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
